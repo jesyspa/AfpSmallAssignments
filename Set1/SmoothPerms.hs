421daf77d33ccef621a38e7031b3af05203ba45b
Anton Golov <jesyspa@gmail.com>
2013-09-11 18:15:50 +0200

    Wrote a little bit of smoothperms.

--- Set1/SmoothPerms.hs
+++ Set1/SmoothPerms.hs
@@ -1,7 +1,23 @@
 module SmoothPerms where
 
-smooth_perms :: Int -> [Int] -> [[Int]]
+--smooth_perms :: Int -> [Int] -> [[Int]]
 
-data Tree a = Node [Tree a] deriving (Show, Eq, Ord)
+data Tree a = Node [(a, Tree a)] deriving (Show, Eq, Ord)
 
+unfoldTree :: (b -> [(a, b)]) -> b -> Tree a
+unfoldTree f b = Node . map g $ f b
+    where g (x, y) = (x, unfoldTree f y)
 
+-- | Given a function, apply it to every element of the list, passing along the
+-- list excluding that element.
+--
+-- Does not preserve order, but we don't care.
+--
+-- There has got to be a better name for this.
+foreachWithRest :: (a -> [a] -> b) -> [a] -> [b]
+foreachWithRest f = go []
+    where go ys [] = []
+          go ys (x:xs) = f x (ys ++ xs) : go (x:ys) xs
+
+buildPermTree :: [a] -> Tree a
+buildPermTree = unfoldTree (foreachWithRest (,))
--- Set1/SmoothPermsTest.hs
+++ Set1/SmoothPermsTest.hs
@@ -1,4 +1,26 @@
-module SmoothPermsTest where
+module Main where
 
 import SmoothPerms
-import QuickCheck
+import Test.QuickCheck
+
+fact :: Int -> Int
+fact n = product [1..n]
+
+-- In my humble opinion, this shouldn't work, and the fact the concat makes it
+-- work is mildly scary.
+allPerms :: [a] -> [[a]]
+allPerms [] = [[]]
+allPerms xs = concat $ foreachWithRest (\y ys -> map (y:) $ allPerms ys) xs
+
+-- We could generalise these to Eq a => [a] -> Bool, but then we'd have to
+-- specify that in main.
+checkForeachWithRest :: [Int] -> Bool
+checkForeachWithRest xs = all f $ foreachWithRest (,) xs
+    where f (x, y) = x `elem` xs && 1 + length y == length xs
+
+-- We can't check all lists; takes too long.
+checkAllPermsLength :: [Int] -> Property
+checkAllPermsLength xs = length xs < 10 ==> length perms == fact (length xs) && xs `elem` perms
+    where perms = allPerms xs
+
+main = quickCheck (checkForeachWithRest .&&. checkAllPermsLength)
